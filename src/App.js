6cc40f33ad823301a422507ad5d1b393ac4e6451
lucasdcsilva04 <jeffersond1silva@hotmail.com>
2023-11-27 21:46:20 -0300

    icones

--- src/App.js
+++ src/App.js
@@ -1,6 +1,195 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
+const formatCurrency = (value) => {
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(value);
+};
+
+const getPaymentMethodInfo = (method) => {
+  const paymentMethods = {
+    cash: { name: "Dinheiro", icon: "💵" },
+    card: { name: "Cartão", icon: "💳" },
+    pix: { name: "PIX", icon: "📱" },
+  };
+
+  return paymentMethods[method] || { name: method, icon: "" };
+};
+
+const FinancialTracker = () => {
+  const [records, setRecords] = useState([]);
+  const [newRecord, setNewRecord] = useState({
+    name: "",
+    value: "",
+    type: "income",
+    paymentMethod: "cash",
+  });
+  const [walletBalance, setWalletBalance] = useState(0);
+  const [incomeTotal, setIncomeTotal] = useState(0);
+  const [expenseTotal, setExpenseTotal] = useState(0);
+
+  useEffect(() => {
+    const storedRecords =
+      JSON.parse(localStorage.getItem("financialRecords")) || [];
+    setRecords(storedRecords);
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      const calculateTotals = () => {
+        const incomeTotal = records
+          .filter((record) => record.type === "income")
+          .reduce((acc, record) => acc + parseFloat(record.value), 0);
+        setIncomeTotal(incomeTotal);
+
+        const expenseTotal = records
+          .filter((record) => record.type === "expense")
+          .reduce((acc, record) => acc + parseFloat(record.value), 0);
+        setExpenseTotal(expenseTotal);
+
+        const newBalance = incomeTotal - expenseTotal;
+        setWalletBalance(newBalance);
+      };
+
+      calculateTotals();
+      localStorage.setItem("financialRecords", JSON.stringify(records));
+    }, 0);
+  }, [records]);
+
+  const addRecord = () => {
+    if (
+      newRecord.name &&
+      newRecord.value &&
+      newRecord.type &&
+      newRecord.paymentMethod
+    ) {
+      setRecords([...records, newRecord]);
+      setNewRecord({
+        name: "",
+        value: "",
+        type: "income",
+        paymentMethod: "cash",
+      });
+    } else {
+      alert("Por favor, preencha todos os campos.");
+    }
+  };
+
+  const deleteRecord = (index) => {
+    const updatedRecords = [...records];
+    updatedRecords.splice(index, 1);
+    setRecords(updatedRecords);
+  };
+
+  const handleValueChange = (e) => {
+    const floatValue = parseFloat(e.target.value.replace(/[^\d]/g, "")) / 100;
+    setNewRecord({ ...newRecord, value: floatValue });
+  };
+
+  return (
+    <Wrapper>
+      <Column>
+        <Form>
+          <h2>Registrar</h2>
+          <label>
+            Nome:
+            <input
+              type="text"
+              value={newRecord.name}
+              onChange={(e) =>
+                setNewRecord({ ...newRecord, name: e.target.value })
+              }
+            />
+          </label>
+          <label>
+            Valor:
+            <input
+              type="text"
+              value={newRecord.value}
+              onChange={handleValueChange}
+              placeholder="0,00"
+            />
+          </label>
+          <label>
+            Tipo:
+            <select
+              value={newRecord.type}
+              onChange={(e) =>
+                setNewRecord({ ...newRecord, type: e.target.value })
+              }
+            >
+              <option value="income">Entrada</option>
+              <option value="expense">Despesa</option>
+            </select>
+          </label>
+          <label>
+            Método de pagamento:
+            <select
+              value={newRecord.paymentMethod}
+              onChange={(e) =>
+                setNewRecord({ ...newRecord, paymentMethod: e.target.value })
+              }
+            >
+              <option value="cash">Dinheiro</option>
+              <option value="card">Cartão</option>
+              <option value="pix">PIX</option>
+            </select>
+          </label>
+          <button onClick={addRecord}>Adicionar</button>
+        </Form>
+      </Column>
+
+      <Column>
+        <h2>Entradas</h2>
+        <RecordsList>
+          {records.map((record, index) =>
+            record.type === "income" ? (
+              <li key={index}>
+                {formatCurrency(record.value)} - {record.name} -{" "}
+                {getPaymentMethodInfo(record.paymentMethod).name}{" "}
+                {getPaymentMethodInfo(record.paymentMethod).icon}
+                <button onClick={() => deleteRecord(index)}>Excluir</button>
+              </li>
+            ) : null
+          )}
+        </RecordsList>
+        <Totals>
+          <p>Total de Entradas: {formatCurrency(incomeTotal.toFixed(2))}</p>
+        </Totals>
+      </Column>
+      <Column>
+        <h2>Despesas</h2>
+        <RecordsList>
+          {records.map((record, index) =>
+            record.type === "expense" ? (
+              <li key={index}>
+                {formatCurrency(record.value)} - {record.name} -{" "}
+                {getPaymentMethodInfo(record.paymentMethod).name}{" "}
+                {getPaymentMethodInfo(record.paymentMethod).icon}
+                <button onClick={() => deleteRecord(index)}>Excluir</button>
+              </li>
+            ) : null
+          )}
+        </RecordsList>
+        <Totals>
+          <p>Total de Despesas: {formatCurrency(incomeTotal.toFixed(2))}</p>
+        </Totals>
+      </Column>
+
+      <Column>
+        <WalletBalance balance={walletBalance}>
+          <h2>Saldo em Carteira</h2>
+          <p>{formatCurrency(walletBalance.toFixed(2))}</p>
+        </WalletBalance>
+      </Column>
+    </Wrapper>
+  );
+};
+
+export default FinancialTracker;
+
 const Wrapper = styled.div`
   display: flex;
   justify-content: space-around;
@@ -92,168 +281,3 @@ const WalletBalance = styled.div`
     color: ${(props) => (props.balance >= 0 ? "green" : "red")};
   }
 `;
-
-const FinancialTracker = () => {
-  const [records, setRecords] = useState([]);
-  const [newRecord, setNewRecord] = useState({
-    name: "",
-    value: "",
-    type: "income",
-    paymentMethod: "cash",
-  });
-  const [walletBalance, setWalletBalance] = useState(0);
-  const [incomeTotal, setIncomeTotal] = useState(0);
-  const [expenseTotal, setExpenseTotal] = useState(0);
-
-  useEffect(() => {
-    const storedRecords =
-      JSON.parse(localStorage.getItem("financialRecords")) || [];
-    setRecords(storedRecords);
-  }, []);
-
-  useEffect(() => {
-    setTimeout(() => {
-      const calculateTotals = () => {
-        const incomeTotal = records
-          .filter((record) => record.type === "income")
-          .reduce((acc, record) => acc + parseFloat(record.value), 0);
-        setIncomeTotal(incomeTotal);
-
-        const expenseTotal = records
-          .filter((record) => record.type === "expense")
-          .reduce((acc, record) => acc + parseFloat(record.value), 0);
-        setExpenseTotal(expenseTotal);
-
-        const newBalance = incomeTotal - expenseTotal;
-        setWalletBalance(newBalance);
-      };
-
-      calculateTotals();
-      localStorage.setItem("financialRecords", JSON.stringify(records));
-    }, 0);
-  }, [records]);
-
-  const addRecord = () => {
-    if (
-      newRecord.name &&
-      newRecord.value &&
-      newRecord.type &&
-      newRecord.paymentMethod
-    ) {
-      setRecords([...records, newRecord]);
-      setNewRecord({
-        name: "",
-        value: "",
-        type: "income",
-        paymentMethod: "cash",
-      });
-    } else {
-      alert("Por favor, preencha todos os campos.");
-    }
-  };
-
-  const deleteRecord = (index) => {
-    const updatedRecords = [...records];
-    updatedRecords.splice(index, 1);
-    setRecords(updatedRecords);
-  };
-
-  return (
-    <Wrapper>
-      <Column>
-        <Form>
-          <h2>Registrar</h2>
-          <label>
-            Nome:
-            <input
-              type="text"
-              value={newRecord.name}
-              onChange={(e) =>
-                setNewRecord({ ...newRecord, name: e.target.value })
-              }
-            />
-          </label>
-          <label>
-            Valor:
-            <input
-              type="number"
-              value={newRecord.value}
-              onChange={(e) =>
-                setNewRecord({ ...newRecord, value: e.target.value })
-              }
-            />
-          </label>
-          <label>
-            Tipo:
-            <select
-              value={newRecord.type}
-              onChange={(e) =>
-                setNewRecord({ ...newRecord, type: e.target.value })
-              }
-            >
-              <option value="income">Entrada</option>
-              <option value="expense">Despesa</option>
-            </select>
-          </label>
-          <label>
-            Pago por:
-            <select
-              value={newRecord.paymentMethod}
-              onChange={(e) =>
-                setNewRecord({ ...newRecord, paymentMethod: e.target.value })
-              }
-            >
-              <option value="cash">Dinheiro</option>
-              <option value="card">Cartão</option>
-              <option value="pix">PIX</option>
-            </select>
-          </label>
-          <button onClick={addRecord}>Adicionar</button>
-        </Form>
-      </Column>
-
-      <Column>
-        <h2>Entradas</h2>
-        <RecordsList>
-          {records.map((record, index) =>
-            record.type === "income" ? (
-              <li key={index}>
-                {record.name} - R${record.value} - {record.paymentMethod}
-                <button onClick={() => deleteRecord(index)}>Excluir</button>
-              </li>
-            ) : null
-          )}
-        </RecordsList>
-        <Totals>
-          <p>Total de Entradas: R${incomeTotal}</p>
-        </Totals>
-      </Column>
-
-      <Column>
-        <h2>Despesas</h2>
-        <RecordsList>
-          {records.map((record, index) =>
-            record.type === "expense" ? (
-              <li key={index}>
-                {record.name} - R${record.value} - {record.paymentMethod}
-                <button onClick={() => deleteRecord(index)}>Excluir</button>
-              </li>
-            ) : null
-          )}
-        </RecordsList>
-        <Totals>
-          <p>Total de Despesas: R${expenseTotal}</p>
-        </Totals>
-      </Column>
-
-      <Column>
-        <WalletBalance balance={walletBalance}>
-          <h2>Saldo em Carteira</h2>
-          <p>R${walletBalance}</p>
-        </WalletBalance>
-      </Column>
-    </Wrapper>
-  );
-};
-
-export default FinancialTracker;
